12faf12112dc166e7838675b9fe88121464f2eca
gamiduwelikanna <gamidukaveen@gmail.com>
2025-06-09 16:13:57 +0530

    routing and controllers - 9th June 2025 - 2

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,22 @@
-export const createSong = (req, res) => {
-    res.send('Admin route with GET method');
+import {Song} from "../models/song.model.js";
+
+export const createSong = async (req, res) => {
+    try{
+        if(!req.files || !req.files.audioFile || !req.files.imageFile){
+            return  res.status(400).send({message: "Please upload all files." });
+        }
+        const {title,artist,albumId,duration} = req.body;
+        const audioFile = req.files.audioFile
+        const imageFile = req.files.imageFile
+        const song = new Song({
+            title,
+            artist,
+            audioUrl,
+            imageUrl,
+            duration,
+
+        })
+    } catch (error){
+
+    }
 }
